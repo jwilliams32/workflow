b98c0688f00179e54e6343ac61a3aff3d21f2d4e
Jeffrey <jeffreywilliams.b@gmail.com>
2018-08-02 09:02:41 -0500

    Changed admin headers

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from .models import Profile, create_user_profile, save_user_profile
 
 # Register your models here.
+admin.site.site_header = "WorkFlow Admin"
+admin.site.site_title = "WorkFlow"
+
+
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display =('location','bio','birth_date',)
     class Meta:
         model = Profile
 admin.site.register(Profile, ProfileAdmin)
-# admin.site.register(create_user_profile)
 
 # admin.site.register(save_user_profile)

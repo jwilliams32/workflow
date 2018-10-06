68d9d597736a5329dc3d3bcde591c7d71085860e
Jeffrey <jeffreywilliams.b@gmail.com>
2018-10-06 15:42:04 -0500

    added image upload functionality for doctors, added file folder to add the images, and updated urls

--- doctors/models.py
+++ doctors/models.py
@@ -4,11 +4,14 @@ from django.urls import reverse
 # Create your models here.
 
 class Doctor(models.Model):
-    # doctor_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
     doctors_name = models.CharField(max_length=500)
     clinic_type = models.CharField(max_length=75)
     description = models.TextField(max_length=1000)
     bio = models.CharField(max_length=500, blank=True)
+    doctor_pic = models.ImageField(blank=True, null=True,
+        upload_to="doctors/%Y/%m/%D/")
+
+
     # test = models.ManyToManyField('tests.Test', related_name='doctor_tests')
 
     def __str__(self):
